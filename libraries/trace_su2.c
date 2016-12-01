b0894ccde9c2cec9ab6961c0ad63acc1fc775892
daschaich <daschaich@gmail.com>
2016-12-01 20:03:15 +0100

    Checkpoint single-precision code that still runs as before

--- libraries/trace_su2.c
+++ libraries/trace_su2.c
@@ -1,17 +1,11 @@
-/*******************  trace_su2.c  (in su2.a) ***************************
-*									*
-* complex trace_su2(a) su2_matrix *a;					*
-* return complex trace of an su2 matrix 				*
-*/
+// -----------------------------------------------------------------
+// Complex trace of a matrix
+// s <-- Tr[a]
 #include "../include/config.h"
 #include "../include/complex.h"
 #include "../include/su2.h"
 
-/* Complex trace of an su2 matrix */
-void trace_su2( su2_matrix *a,complex *s) {
-    complex tr;
-    CADD(a->e[0][0],a->e[1][1],tr);
-    s =&tr;
-    //CADD(t1,a->e[2][2],t2);
-    
+void trace_su2(su2_matrix *a, complex *s) {
+  CADD(a->e[0][0], a->e[1][1], *s);
 }
+// -----------------------------------------------------------------
