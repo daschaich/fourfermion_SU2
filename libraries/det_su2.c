753ce62163fbc99f0f8cc73ce2ce357d28d6500e
Nouman Tariq <nompire@hotmail.com>
2016-12-02 14:21:03 -0500

    Update det_su2.c

--- libraries/det_su2.c
+++ libraries/det_su2.c
@@ -11,22 +11,13 @@
 /* FIX THIS - more efficient to take cross product of first two
    rows, dot with third. */
 void det_su2( su2_matrix *a,double *det) {
-//register complex cc,dd;
+register complex cc,dd,s;
  double d;
-       
-    
-    //CMUL(a->e[0][0],a->e[1][1],cc);
-    
-    
-    
-    //CMUL(a->e[0][1],a->e[1][0],dd);
-    
-    //CSUB(cc,dd,s);
-
-    d = (a->e[0][0].real * a->e[1][1].real) - ( a->e[0][0].imag * a->e[1][1].imag) - ( a->e[0][1].real * a->e[1][0].real) + (a->e[0][1].imag * a->e[1][0].imag); 
+    CMUL(a->e[0][0],a->e[1][1],cc);
+    CMUL(a->e[0][1],a->e[1][0],dd);
+    CSUB(cc,dd,s);
+    d= s.real;
+    //d = (a->e[0][0].real * a->e[1][1].real) - ( a->e[0][0].imag * a->e[1][1].imag) - ( a->e[0][1].real * a->e[1][0].real) + (a->e[0][1].imag * a->e[1][0].imag); 
     det =&d;
-    printf("Det = %.1f\n",d);
-    
-
-    
+    //printf("Det = %.1f\n",d);
 }
